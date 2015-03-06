02529788201e6f45bf18750149875f32c9471010
Allan Lasser <lasser.allan@gmail.com>
2015-03-06 13:50:17 -0500

    creates a Record object

---
+++ whitewash/MRRecord.h
@@ -0,0 +1,13 @@
+//
+//  MRRecord.h
+//  Whitewash
+//
+//  Created by Allan Lasser on 3/6/15.
+//  Copyright (c) 2015 MuckRock. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MRRecord : NSObject
+
+@end
---
+++ whitewash/MRRecord.m
@@ -0,0 +1,13 @@
+//
+//  MRRecord.m
+//  Whitewash
+//
+//  Created by Allan Lasser on 3/6/15.
+//  Copyright (c) 2015 MuckRock. All rights reserved.
+//
+
+#import "MRRecord.h"
+
+@implementation MRRecord
+
+@end
