0e0e64de4cc085c895dc195fe9b6bf959650a94e
Allan Lasser <lasser.allan@gmail.com>
2015-03-10 11:50:00 -0400

    installs OCMock testing library

--- Pods/Target Support Files/Pods-whitewashTests/Pods-whitewashTests-environment.h
+++ Pods/Target Support Files/Pods-whitewashTests/Pods-whitewashTests-environment.h
@@ -6,6 +6,12 @@
 // project.
 
 
+// OCMock
+#define COCOAPODS_POD_AVAILABLE_OCMock
+#define COCOAPODS_VERSION_MAJOR_OCMock 3
+#define COCOAPODS_VERSION_MINOR_OCMock 1
+#define COCOAPODS_VERSION_PATCH_OCMock 2
+
 // Tweaks
 #define COCOAPODS_POD_AVAILABLE_Tweaks
 #define COCOAPODS_VERSION_MAJOR_Tweaks 1
