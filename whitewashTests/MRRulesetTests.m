5663f42bc73eb1401aa00d96054784fd464c45b7
Allan Lasser <lasser.allan@gmail.com>
2015-03-10 11:28:05 -0400

    refactors ruleset tests

--- whitewashTests/MRRulesetTests.m
+++ whitewashTests/MRRulesetTests.m
@@ -12,6 +12,9 @@
 
 @interface MRRulesetTests : XCTestCase
 
+@property (nonatomic) MRRuleset *rules;
+@property (nonatomic) NSObject *specificRule;
+
 @end
 
 @implementation MRRulesetTests
@@ -19,65 +22,62 @@
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    _rules = [[MRRuleset alloc] init];
+    _specificRule = [[NSObject alloc] init];
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
+    _rules = nil;
+    _specificRule = nil;
 }
 
 - (void)testInit {
-    MRRuleset *rules = [[MRRuleset alloc] init];
-    XCTAssertNotNil(rules);
+    XCTAssertNotNil(_rules);
 }
 
 - (void)testRuleArrays {
-    MRRuleset *rules = [[MRRuleset alloc] init];
-    XCTAssertNotNil(rules.startState);
-    XCTAssertEqual([rules.startState count], 0);
-    XCTAssertNotNil(rules.moves);
-    XCTAssertEqual([rules.moves count], 0);
-    XCTAssertNotNil(rules.endState);
-    XCTAssertEqual([rules.endState count], 0);
+    XCTAssertNotNil(_rules.startState);
+    XCTAssertEqual([_rules.startState count], 0);
+    XCTAssertNotNil(_rules.moves);
+    XCTAssertEqual([_rules.moves count], 0);
+    XCTAssertNotNil(_rules.endState);
+    XCTAssertEqual([_rules.endState count], 0);
 }
 
 - (void)testAddingRule {
-    MRRuleset *rules = [[MRRuleset alloc] init];
-    NSObject *specificRule = [[NSObject alloc] init];
-    [rules addStartStateRule:specificRule];
-    XCTAssertEqualObjects(rules.startState[0], specificRule);
-    [rules addEndStateRule:specificRule];
-    XCTAssertEqualObjects(rules.endState[0], specificRule);
-    [rules addMoveRule:specificRule];
-    XCTAssertEqualObjects(rules.moves[0], specificRule);
+    [_rules addStartStateRule:_specificRule];
+    XCTAssertEqualObjects(_rules.startState[0], _specificRule);
+    [_rules addEndStateRule:_specificRule];
+    XCTAssertEqualObjects(_rules.endState[0], _specificRule);
+    [_rules addMoveRule:_specificRule];
+    XCTAssertEqualObjects(_rules.moves[0], _specificRule);
 }
 
 - (void)testRemovingRule {
-    MRRuleset *rules = [[MRRuleset alloc] init];
-    NSObject *specificRule = [[NSObject alloc] init];
-    XCTAssertNoThrow([rules removeStartStateRule:specificRule]);
-    XCTAssertNoThrow([rules removeEndStateRule:specificRule]);
-    XCTAssertNoThrow([rules removeMoveRule:specificRule]);
-    [rules addStartStateRule:specificRule];
-    [rules addEndStateRule:specificRule];
-    [rules addMoveRule:specificRule];
-    [rules removeStartStateRule:specificRule];
-    XCTAssertEqual([rules.startState count], 0);
-    [rules removeEndStateRule:specificRule];
-    XCTAssertEqual([rules.endState count], 0);
-    [rules removeMoveRule:specificRule];
-    XCTAssertEqual([rules.moves count], 0);
+    XCTAssertNoThrow([_rules removeStartStateRule:_specificRule]);
+    XCTAssertNoThrow([_rules removeEndStateRule:_specificRule]);
+    XCTAssertNoThrow([_rules removeMoveRule:_specificRule]);
+    [_rules addStartStateRule:_specificRule];
+    [_rules addEndStateRule:_specificRule];
+    [_rules addMoveRule:_specificRule];
+    [_rules removeStartStateRule:_specificRule];
+    XCTAssertEqual([_rules.startState count], 0);
+    [_rules removeEndStateRule:_specificRule];
+    XCTAssertEqual([_rules.endState count], 0);
+    [_rules removeMoveRule:_specificRule];
+    XCTAssertEqual([_rules.moves count], 0);
 }
 
 - (void)testAddingRules {
-    MRRuleset *rules = [[MRRuleset alloc] init];
     NSArray *specificRules = @[@"Stop", @"Go", @"Turn Left", @"Turn Right"];
-    [rules addStartStateRules:specificRules];
-    XCTAssertEqual([rules.startState count], 4);
-    [rules addEndStateRules:specificRules];
-    XCTAssertEqual([rules.endState count], 4);
-    [rules addMoveRules:specificRules];
-    XCTAssertEqual([rules.moves count], 4);
+    [_rules addStartStateRules:specificRules];
+    XCTAssertEqual([_rules.startState count], 4);
+    [_rules addEndStateRules:specificRules];
+    XCTAssertEqual([_rules.endState count], 4);
+    [_rules addMoveRules:specificRules];
+    XCTAssertEqual([_rules.moves count], 4);
 }
 
 @end
