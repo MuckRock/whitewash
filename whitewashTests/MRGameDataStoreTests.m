d82a3c07e0a9541498e4490ee6a929c95dbe799a
Allan Lasser <lasser.allan@gmail.com>
2015-03-10 11:32:40 -0400

    refactors game data store tests

--- whitewashTests/MRGameDataStoreTests.m
+++ whitewashTests/MRGameDataStoreTests.m
@@ -12,6 +12,10 @@
 
 @interface MRGameDataStoreTests : XCTestCase
 
+@property (nonatomic) MRGameDataStore *store;
+@property (nonatomic) NSObject *data;
+@property (nonatomic) NSObject *moreData;
+
 @end
 
 @implementation MRGameDataStoreTests
@@ -19,50 +23,53 @@
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    _store = [[MRGameDataStore alloc] init];
+    _data = [[NSObject alloc] init];
+    _moreData = [[NSObject alloc] init];
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
+    _store = nil;
+    _data = nil;
+    _moreData = nil;
 }
 
 - (void)testInit {
-    MRGameDataStore *store = [[MRGameDataStore alloc] init];
-    XCTAssertNotNil(store, @"GameDataStore fails to return object on init");
+    XCTAssertNotNil(_store,
+                    @"GameDataStore fails to return object on init");
 }
 
 - (void)testGameData {
-    MRGameDataStore *store = [[MRGameDataStore alloc] init];
-    XCTAssertNotNil(store.gameData, @"GameDataStore fails to create gameData array on init");
-    XCTAssertEqual([store.gameData count], 0, @"gameData is not initially empty");
+    XCTAssertNotNil(_store.gameData,
+                    @"GameDataStore fails to create gameData array on init");
+    XCTAssertEqual([_store.gameData count], 0,
+                   @"gameData is not initially empty");
 }
 
 - (void)testAddData {
-    MRGameDataStore *store = [[MRGameDataStore alloc] init];
-    NSObject *data = [[NSObject alloc] init];
-    [store addData:data];
-    XCTAssertEqual([store.gameData count], 1, @"data does not get added to gameData array");
-    XCTAssertEqualObjects(store.gameData[0], data, @"wrong data is added to gameData array");
+    [_store addData:_data];
+    XCTAssertEqual([_store.gameData count], 1,
+                   @"data does not get added to gameData array");
+    XCTAssertEqualObjects(_store.gameData[0], _data,
+                          @"wrong data is added to gameData array");
 }
 
 - (void)testRemoveData {
-    MRGameDataStore *store = [[MRGameDataStore alloc] init];
-    NSObject *data = [[NSObject alloc] init];
-    XCTAssertNoThrow([store removeData:data]);
-    [store addData:data];
-    [store removeData:data];
-    XCTAssertEqual([store.gameData count], 0);
+    XCTAssertNoThrow([_store removeData:_data]);
+    [_store addData:_data];
+    [_store removeData:_data];
+    XCTAssertEqual([_store.gameData count], 0);
 }
 
 - (void)testMoveData {
-    MRGameDataStore *store = [[MRGameDataStore alloc] init];
-    NSObject *data1 = [[NSObject alloc] init];
-    NSObject *data2 = [[NSObject alloc] init];
-    [store addData:data1];
-    [store addData:data2];
-    XCTAssertEqualObjects(store.gameData[0], data1);
-    [store moveDataAtIndex:1 toIndex:0];
-    XCTAssertEqualObjects(store.gameData[0], data2, @"Failed to move data to proper index");
+    [_store addData:_data];
+    [_store addData:_moreData];
+    XCTAssertEqualObjects(_store.gameData[0], _data);
+    [_store moveDataAtIndex:1 toIndex:0];
+    XCTAssertEqualObjects(_store.gameData[0], _moreData,
+                          @"Failed to move data to proper index");
 }
 
 @end
