3929572153dafe15273ad28e67f4fca90128e661
Allan Lasser <lasser.allan@gmail.com>
2015-03-11 15:18:48 -0400

    adds MRGameData object

---
+++ whitewash/MRGameData.h
@@ -0,0 +1,13 @@
+//
+//  MRGameData.h
+//  Whitewash
+//
+//  Created by Allan Lasser on 3/11/15.
+//  Copyright (c) 2015 MuckRock. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MRGameData : NSObject
+
+@end
---
+++ whitewash/MRGameData.m
@@ -0,0 +1,13 @@
+//
+//  MRGameData.m
+//  Whitewash
+//
+//  Created by Allan Lasser on 3/11/15.
+//  Copyright (c) 2015 MuckRock. All rights reserved.
+//
+
+#import "MRGameData.h"
+
+@implementation MRGameData
+
+@end
