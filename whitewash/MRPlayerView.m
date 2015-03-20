8c048b9ead44506df321040a4446e07a8e56b6ca
Allan Lasser <lasser.allan@gmail.com>
2015-03-20 13:02:17 -0400

    adds a MRPlayerView class, idk why actually

---
+++ whitewash/MRPlayerView.m
@@ -0,0 +1,21 @@
+//
+//  MRPlayerView.m
+//  Whitewash
+//
+//  Created by Allan Lasser on 3/20/15.
+//  Copyright (c) 2015 MuckRock. All rights reserved.
+//
+
+#import "MRPlayerView.h"
+
+@implementation MRPlayerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
