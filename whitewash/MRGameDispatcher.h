49ea6ca3f1e6af7eada914b971def138844efb10
Allan Lasser <lasser.allan@gmail.com>
2015-03-30 15:44:57 -0400

    creates a game dispatcher with hardcoded values

---
+++ whitewash/MRGameDispatcher.h
@@ -0,0 +1,17 @@
+//
+//  MRGameDispatcher.h
+//  Whitewash
+//
+//  Created by Allan Lasser on 3/30/15.
+//  Copyright (c) 2015 MuckRock. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class MRGame;
+
+@interface MRGameDispatcher : NSObject
+
+- (MRGame *)newGame;
+
+@end
